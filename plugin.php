382f06b3c912cd297606470b43fb0542ce409c3c
ilhamsj <ilhamsaputrajati@gmail.com>
2023-10-05 22:05:02 +0700

    init

---
+++ plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: Movie
+ * Plugin URI: https://github.com/iam-atomic/movie
+ * GitHub Plugin URI: https://github.com/iam-atomic/movie
+ * Description: GraphQL API for WordPress
+ * Author: Movie
+ * Author URI: http://www.github.com/iam-atomic
+ * Version: 1.16.0
+ * Text Domain: movie
+ * Domain Path: /languages/
+ * Requires at least: 5.0
+ * Tested up to: 6.2
+ * Requires PHP: 7.1
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package  Movie
+ * @category Core
+ * @author   Movie
+ * @version  1.16.0
+ */
